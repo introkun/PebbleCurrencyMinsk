66bb394dcfab2838875dc6ae4faa69cf497b0f37
introkun <introkun@gmail.com>
2015-01-28 22:35:05 +0300

    Display EUR and EUR/USD conversion rates

--- src/app.js
+++ src/app.js
@@ -1,27 +1,48 @@
 var UI = require('ui');
 var ajax = require('ajax');
+var Vibe = require('ui/vibe');
+var Accel = require('ui/accel');
 
 var main = new UI.Card({
   title: 'kurs.onliner.by',
   subtitle: 'Updating...',
+  scrollable: true
 });
 
 main.show();
+Accel.init();
 
 var URL = 'https://kurs-onliner-by.herokuapp.com/';
+var byrCurrency = " BYR\n";
 
-// Make the request
-ajax(
-  {
-    url: URL,
-    type: 'json'
-  },
-  function(data) {
-    main.subtitle("");
-    main.body("Bank buys $$:\n" + data.usdSell + " BYR\nBank sells $$:\n" + data.usdBuy+ " BYR");
-  },
-  function(error) {
-    // Failure!
-    console.log('No $$$, sorry');
-  }
-);
+var updateRates = function() {
+  main.subtitle("Updating...");
+  ajax(
+    {
+      url: URL,
+      type: 'json'
+    },
+    function(data) {
+      main.subtitle("");
+      main.body("");
+      var content = "NBRB $:\n" + data.usdNbrb + byrCurrency;
+      content += "Bank buys $:\n" + data.usdSell + byrCurrency;
+      content += "Bank sells $:\n" + data.usdBuy+ byrCurrency;
+      content += "NBRB €:\n" + data.eurNbrb + byrCurrency;
+      content += "Bank buys €:\n" + data.eurSell + byrCurrency;
+      content += "Bank sells €:\n" + data.eurBuy+ byrCurrency;
+      main.body(content);
+      Vibe.vibrate('short');
+    },
+    function(error) {
+      // Failure!
+      console.log('No $$$, sorry');
+    }
+  );
+};
+
+updateRates();
+
+Accel.on('tap', function(e) {
+  updateRates();
+});
